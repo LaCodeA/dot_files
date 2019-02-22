594dea2fa4228639580a385f3b15358ddf5efc87
llefler <llefler@etsy.com>
2019-02-22 19:54:13 +0000

    Updating Etsy VM prompt to reflect vm name

--- bash_env_specific_etsy_vm.sh
+++ bash_env_specific_etsy_vm.sh
@@ -1,6 +1,6 @@
 alias dropbox='~/.dropbox-dist/dropboxd start'
 alias tailphp='tail -f -n 500 /var/log/httpd/php.log'
-PS1_PREFIX="\[\033[0;92m\][My Vm]\[\033[0m\] "
+PS1_PREFIX="\[\033[0;92m\][llefler.vms]\[\033[0m\] "
 source ~/development/bin/xdebug_toggle
 alias grebasem='git fetch origin master:master && git rebase master'
 cd ~/development/Etsyweb/
