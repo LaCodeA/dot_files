79f6648fc461c0ed820cd436f65b856cad89342e
llefler <llefler@etsy.com>
2019-08-06 13:21:57 -0400

    Adding view commit alias

--- bash_env_specific_etsy_home.sh
+++ bash_env_specific_etsy_home.sh
@@ -3,6 +3,8 @@ export ANDROID_HOME=/usr/local/share/android-sdk
 export GITHUB_URL=https://github.etsycorp.com
 alias ggist="git diff | gist -o -t diff"
 alias greseth="git fetch && git reset --hard origin/\$(git rev-parse --abbrev-ref HEAD)"
+alias mycommits="gl --since='3 months ago' --author=$(git config user.name)
+alias capweb="gl --since='5 weeks ago' --author=$(git config user.name)
 
 COLOR_GREY="\[\033[0;37m\]"
 COLOR_RESET="\[\033[0m\]"
